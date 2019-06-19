bfb5d89ff109de28d9a42074807e9e58bb4a0cfd
wesrer <sayakchatterjee1309@gmail.com>
2019-06-19 17:49:14 +0530

    Catching task_type and action_type mismatch now; fixes #23

--- src/CLI/CLI_fire.py
+++ src/CLI/CLI_fire.py
@@ -35,79 +35,82 @@ def perform_actions(task_type,
                     args,
                     weight=3):
 
-    if task_type not in ["all", "test", "pandas"]:
-        obj = parse_task(task_type=task_type)
+    try:
+        if task_type not in ["all", "test", "pandas"]:
+            obj = parse_task(task_type=task_type)
 
-    elif task_type == "pandas":
-        app.get_pandas().print_active()
-        return
+        elif task_type == "pandas":
+            app.get_pandas().print_active()
+            return
 
-    elif action_type in ["list", "view", "active"]:
-        PrettyPrinter.print_header("habits")
-        perform_actions(task_type="habit", action_type="list", args=[])
+        elif action_type in ["list", "view", "active"]:
+            PrettyPrinter.print_header("habits")
+            perform_actions(task_type="habit", action_type="list", args=[])
 
-        PrettyPrinter.print_header("tasks")
-        perform_actions(task_type="task", action_type="list", args=[])
-        return
+            PrettyPrinter.print_header("tasks")
+            perform_actions(task_type="task", action_type="list", args=[])
+            return
 
-    elif task_type == "test":
-        print(UserInputParser.generate_task_string(args=args))
-        return
+        elif task_type == "test":
+            print(UserInputParser.generate_task_string(args=args))
+            return
 
-    if action_type == "add":
-        task_string = UserInputParser.generate_task_string(args=args)
-        obj.add(task_string=task_string)
+        if action_type == "add":
+            task_string = UserInputParser.generate_task_string(args=args)
+            obj.add(task_string=task_string)
 
-    elif action_type in ['update', 'edit']:
-        # TODO: implement updating priorities / weight / dates
-        try:
-            if type(args[0]) is int:
-                id_to_update = int(args[0])
-                task_string = UserInputParser.generate_task_string(args[1:])
+        elif action_type in ['update', 'edit']:
+            # TODO: implement updating priorities / weight / dates
+            try:
+                if type(args[0]) is int:
+                    id_to_update = int(args[0])
+                    task_string = UserInputParser.generate_task_string(args[1:])
 
-                obj.update(id_to_update=id_to_update,
-                           option_to_update="task_string",
-                           updated_value=task_string)
-            elif "id" in args:
-                raise NotImplementedError
-            else:
-                raise TypeError
-        except TypeError as err:
-            print(err)
-            sys.exit("update needs an id to update")
-        except NotImplementedError:
-            sys.exit("Using id: isn't implemented yet. Sorry!")
+                    obj.update(id_to_update=id_to_update,
+                               option_to_update="task_string",
+                               updated_value=task_string)
+                elif "id" in args:
+                    raise NotImplementedError
+                else:
+                    raise TypeError
+            except TypeError as err:
+                print(err)
+                sys.exit("update needs an id to update")
+            except NotImplementedError:
+                sys.exit("Using id: isn't implemented yet. Sorry!")
 
-    elif action_type in ["active", "list", "view"]:
-        PrettyPrinter.pprint(task_dict=obj.get_active(), task_type=task_type)
+        elif action_type in ["active", "list", "view"]:
+            PrettyPrinter.pprint(task_dict=obj.get_active(), task_type=task_type)
 
-    elif action_type == "completed":
-        if len(args) == 0:
-            PrettyPrinter.pprint(task_dict=obj.get_completed(), task_type=task_type)
+        elif action_type == "completed":
+            if len(args) == 0:
+                PrettyPrinter.pprint(task_dict=obj.get_completed(), task_type=task_type)
 
-    elif action_type == "delete" or action_type == "del":
-        task_id = args[0]
+        elif action_type == "delete" or action_type == "del":
+            task_id = args[0]
 
-        Actions.delete(task_id=task_id, args=args, task_object=obj, task_type=task_type)
+            Actions.delete(task_id=task_id, args=args, task_object=obj, task_type=task_type)
 
-    elif action_type == "mark":
-        ids_to_mark = UserInputParser.generate_list_of_ids(args)
+        elif action_type == "mark":
+            ids_to_mark = UserInputParser.generate_list_of_ids(args)
 
-        if task_type == "task":
-            obj.mark_as_completed(list_of_ids_to_mark_as_completed=ids_to_mark)
-        elif task_type == "habit":
-            obj.mark_as_completed(list_of_ids_to_mark_as_completed=ids_to_mark)
+            if task_type == "task":
+                obj.mark_as_completed(list_of_ids_to_mark_as_completed=ids_to_mark)
+            elif task_type == "habit":
+                obj.mark_as_completed(list_of_ids_to_mark_as_completed=ids_to_mark)
 
-    # TODO: implement unmarking inactive habits
-    elif action_type == "unmark":
-        ids_to_unmark = UserInputParser.generate_list_of_ids(args)
-        if task_type == "task":
-            obj.unmark_completed(list_of_ids_to_unmark=ids_to_unmark)
-        elif task_type == "habit":
-            obj.unmark_completed(list_of_ids_to_unmark=ids_to_unmark)
+        # TODO: implement unmarking inactive habits
+        elif action_type == "unmark":
+            ids_to_unmark = UserInputParser.generate_list_of_ids(args)
+            if task_type == "task":
+                obj.unmark_completed(list_of_ids_to_unmark=ids_to_unmark)
+            elif task_type == "habit":
+                obj.unmark_completed(list_of_ids_to_unmark=ids_to_unmark)
 
-    else:
-        raise ValueError(f"{action_type} is not a valid action on {task_type}")
+        else:
+            raise ValueError
+    except ValueError:
+        sys.exit(f"{action_type} is not a valid action on {task_type}")
 
 
 def parse_task(task_type: str):

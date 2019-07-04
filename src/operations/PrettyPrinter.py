02d75661bd5df43d229706d14931ceb35722cc89
wesrer <sayakchatterjee1309@gmail.com>
2019-07-04 12:35:51 +0530

    pretty printer now sorts descending for the things that are supposed to be sorted descending; fixes #61

--- src/operations/PrettyPrinter.py
+++ src/operations/PrettyPrinter.py
@@ -42,6 +42,7 @@ class PrettyPrinter:
         date_headers = []
         columns_to_print = task_dataframe
         sort_by = ['priority', 'weight']
+        sort_ascending = [False, False]
 
         # FIXME: these need to updated to cover custom queues
         if task_type in ["task", "tasks"]:
@@ -49,11 +50,13 @@ class PrettyPrinter:
                 date_headers = ['assignedOn', 'scheduledStart', 'completeBy']
                 columns_to_print = task_dataframe[['taskString', 'completeBy', 'priority', 'weight']]
                 sort_by.insert(0, 'completeBy')  # adding to the beginning of the list
+                sort_ascending.insert(0, False)
 
             elif queue == "inactive":
                 date_headers = ['assignedOn', 'scheduledStart']
                 columns_to_print = task_dataframe[['taskString', 'scheduledStart', 'priority', 'weight']]
                 sort_by.append('scheduledStart')
+                sort_ascending.append(True)
 
             elif queue == "completed":
                 # FIXME: add 'completedOn' when added to the data points
@@ -64,11 +67,12 @@ class PrettyPrinter:
                 date_headers = ['assignedOn', 'scheduledStart']
             elif queue == "inactive":
                 date_headers = ['assignedOn', 'lastCompletedOn']
+            # FIXME: add 'lastCompletedOn' for the 'completed' queue, and then display it
             columns_to_print = task_dataframe[['taskString', 'priority', 'weight', 'refreshRate']]
 
         for x in date_headers:
             task_dataframe[x] = pd.to_datetime(task_dataframe[x])
 
         # FIXME: longTermTasks not implemented
-        print(columns_to_print.sort_values(sort_by))
+        print(columns_to_print.sort_values(sort_by, ascending=sort_ascending))
 
